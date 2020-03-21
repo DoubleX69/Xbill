7d9db819be6a25ea96c970264c2ef1dc55418aaf
DoubleX69 <xiexiangh69@163.com>
2020-03-21 17:01:15 +0800

    add:增加判断csv文件是否是icbc账单

--- Recorder/RecordFactory.py
+++ Recorder/RecordFactory.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from Recorder.AlipayRecorder import AlipayRecorder
+
+from Recorder.ICBCRecorder import ICBCRecorder
 import csv
 
 
@@ -17,11 +19,27 @@ def is_alipay_file(file_path) -> bool:
     return is_alipay
 
 
-def get_recorder(file_path) -> Union[AlipayRecorder, None]:
+def is_icbc_file(file_path) -> bool:
+    is_icbc = False
+    try:
+        with open(file_path, encoding='UTF-8') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                for val in row:
+                    if '明细查询文件下载' in val:
+                        is_icbc = True
+    except UnicodeDecodeError:
+        is_icbc = False
+    return is_icbc
+
+
+def get_recorder(file_path) -> Union[AlipayRecorder, ICBCRecorder, None]:
     if is_alipay_file(file_path):
         print("Read Alipay Records")
         return AlipayRecorder(file_path)
-
+    elif is_icbc_file(file_path):
+        print("Read ICBC Records")
+        return ICBCRecorder(file_path)
     else:
         print("Read Unknown Records")
         return None
