28b2c47cb7a60b857edb7042c57fdbd054cff9ff
DoubleX69 <xiexiangh69@163.com>
2020-03-21 16:50:51 +0800

    add:增加创建表icbc

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,7 +1,8 @@
 from .BaseModel import database
 from .AlipayBill import AlipayBill
+from .ICBCBill import ICBCBill
 from .XBill import XBill
 
 
 def create_table():
-    database.create_tables([AlipayBill, XBill], safe=True)
+    database.create_tables([AlipayBill, ICBCBill, XBill], safe=True)
