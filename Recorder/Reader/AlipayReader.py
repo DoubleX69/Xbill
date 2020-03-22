bad958fb305ea694c922ddcdd0157260b2f2404f
DoubleX69 <xiexiangh69@163.com>
2020-03-22 17:43:43 +0800

    refactor:重构csv文件内容到Model的逻辑

--- Recorder/Reader/AlipayReader.py
+++ Recorder/Reader/AlipayReader.py
@@ -5,62 +5,28 @@ from Models.AlipayBill import AlipayBill
 
 from Helper.Utilities import *
 
-import csv
-
 
 class AlipayReader(BaseReader):
+
     titles = ['交易号', '商家订单号', '交易创建时间', '付款时间', '最近修改时间', '交易来源地', '类型', '交易对方', '商品名称', '金额（元）', '收/支', '交易状态',
               '服务费（元）', '成功退款（元）', '备注', '资金状态']
 
-    def __init__(self, file_path):
-        super(AlipayReader, self).__init__(file_path)
+    def __init__(self, rows):
+        super(AlipayReader, self).__init__(rows)
 
-    def is_standard_file(self, row=None) -> bool:
-        if row is None:
-            row = []
-        if len(row) < len(self.titles):
-            return False
-        else:
-            for idx in range(len(self.titles)):
-                if self.titles[idx] not in row[idx]:
-                    return False
-            return True
-
-    def read_line(self, row=None) -> Optional[AlipayBill]:
-        if row is None:
-            row = []
-        if len(row) < len(self.titles):
-            return None
-        else:
-            record = AlipayBill()
-            record.trans_id = row[0].strip()
-            record.order_id = row[1].strip()
-            record.create_time = str_to_datetime(row[2].strip())
-            record.modify_time = str_to_datetime(row[4].strip())
-            record.source = row[5].strip()
-            record.trans_type = row[6].strip()
-            record.trader_name = row[7].strip()
-            record.product_name = row[8].strip()
-            record.amount = float(row[9].strip())
-            record.trans_status = row[10].strip()
-            record.service_fee = float(row[12].strip())
-            record.remarks = row[14].strip()
-            record.fund_status = row[15].strip()
-            return record
-
-    def read(self):
-        records = []
-        is_standard = False
-        with open(self.file_path, encoding='GBK') as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                if self.is_standard_file(row):
-                    is_standard = True
-                    break
-            if is_standard:
-                for row in reader:
-                    record = self.read_line(row)
-                    if record is not None:
-                        records.append(record)
-        records.reverse()
-        return records
+    def to_model(self, row):
+        bill = AlipayBill()
+        bill.trans_id = row[0].strip()
+        bill.order_id = row[1].strip()
+        bill.create_time = str_to_datetime(row[2].strip())
+        bill.modify_time = str_to_datetime(row[4].strip())
+        bill.source = row[5].strip()
+        bill.trans_type = row[6].strip()
+        bill.trader_name = row[7].strip()
+        bill.product_name = row[8].strip()
+        bill.amount = float(row[9].strip())
+        bill.trans_status = row[10].strip()
+        bill.service_fee = float(row[12].strip())
+        bill.remarks = row[14].strip()
+        bill.fund_status = row[15].strip()
+        return bill
--- Recorder/Reader/BaseReader.py
+++ Recorder/Reader/BaseReader.py
@@ -1,12 +1,58 @@
 class BaseReader(object):
-    def __init__(self, file_path):
-        self.file_path = file_path
+    titles = ['Base']
 
-    def is_standard_file(self, row=None):
-        pass
+    def __init__(self, rows):
+        self.rows = rows
+        self.start = -1
+        self.end = -1
 
-    def read_line(self, row=None):
-        pass
+    def is_title_row(self, row=None) -> bool:
+        if row is None:
+            return False
+
+        if len(row) < len(self.titles):
+            return False
+        else:
+            for idx in range(len(self.titles)):
+                if self.titles[idx] not in row[idx]:
+                    return False
+            return True
+
+    def locate_begin_pos(self):
+        line_num = 0
+
+        for row in self.rows:
+            # title的下一行 ，才是数据开始的行数
+            line_num += 1
+            if self.is_title_row(row):
+                self.start = line_num
+                return
+        self.start = -1
+
+    def locate_finish_pos(self):
+        for idx in range(self.start, len(self.rows)):
+            if len(self.rows[idx]) < len(self.titles):
+                self.end = idx
+                break
+
+        if -1 == self.end:
+            self.end = len(self.rows)
+
+    def to_model(self, row):
+        raise NotImplementedError
 
     def read(self):
-        pass
+
+        self.locate_begin_pos()
+        self.locate_finish_pos()
+
+        bills = []
+
+        if self.start != -1 and self.end != -1:
+
+            for idx in range(self.start, self.end):
+                bill = self.to_model(self.rows[idx])
+                bills.append(bill)
+
+        bills.reverse()
+        return bills
